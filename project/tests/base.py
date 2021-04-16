60493004469681c065785c7008777c0724040455
nikkeng711 <nikkeng711@gmail.com>
2021-04-16 22:54:21 +0530

    commit

---
+++ project/tests/base.py
@@ -0,0 +1,22 @@
+
+
+
+from flask_testing import TestCase
+
+from project.server import app, db
+
+
+class BaseTestCase(TestCase):
+   
+
+    def create_app(self):
+        app.config.from_object('project.server.config.TestingConfig')
+        return app
+
+    def setUp(self):
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
